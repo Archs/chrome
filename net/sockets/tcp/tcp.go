e5d6d11450261bc3cab8c4ebc2181e4c8f7ef353
Archs <sun@archs.tk>
2015-05-19 13:20:01 +0800

    update

--- net/sockets/tcp/tcp.go
+++ net/sockets/tcp/tcp.go
@@ -5,27 +5,27 @@ import (
 )
 
 var (
-	tcp = js.Global
+	tcp = js.Global.Get("chrome").Get("sockets").Get("tcp")
 )
 
 // chrome.sockets.tcp.create( SocketProperties properties, function callback)
 // Creates a TCP socket.
-
+//
 // Parameters
 // SocketProperties	(optional) properties
 // The socket properties (optional).
-
+//
 // function	callback
 // Called when the socket has been created.
-
+//
 // The callback parameter should be a function that looks like this:
-
+//
 // function(object createInfo) {...};
-// object	createInfo
-// The result of the socket creation.
-
-// integer	socketId
-// The ID of the newly created socket. Note that socket IDs created from this API are not compatible with socket IDs created from other APIs, such as the deprecated socket API.
-func (t *Tcp) Create(p SocketProperties, callback func(CreateInfo)) {
-	t.Object.Call("create", p, callback)
+//    object	createInfo
+//       The result of the socket creation.
+//
+//    integer	socketId
+//        The ID of the newly created socket. Note that socket IDs created from this API are not compatible with socket IDs created from other APIs, such as the deprecated socket API.
+func Create(p SocketProperties, callback func(*CreateInfo)) {
+	tcp.Call("create", p, callback)
 }
