4902ebe751437861d0db1459aaf8e548263f3f55
Archs <sun@archs.tk>
2015-05-19 12:55:32 +0800

    chrome.sockets.tcp added

--- net/sockets/tcp/tcp.go
+++ net/sockets/tcp/tcp.go
@@ -1,6 +1,7 @@
-package net
+package tcp
 
 import (
+	"Archs/chrome/net/sockets"
 	"github.com/gopherjs/gopherjs/js"
 )
 
@@ -26,6 +27,303 @@ var (
 //
 //    integer	socketId
 //        The ID of the newly created socket. Note that socket IDs created from this API are not compatible with socket IDs created from other APIs, such as the deprecated socket API.
-func Create(p SocketProperties, callback func(*CreateInfo)) {
+func Create(p sockets.SocketProperties, callback func(*sockets.CreateInfo)) {
 	tcp.Call("create", p, callback)
+}
+
+// chrome.sockets.tcp.connect(integer socketId, string peerAddress, integer peerPort, function callback)
+// Connects the socket to a remote machine. When the connect operation completes successfully, onReceive events are raised when data is received from the peer. If a network error occurs while the runtime is receiving packets, a onReceiveError event is raised, at which point no more onReceive event will be raised for this socket until the resume method is called.
+//
+// Parameters
+// integer	socketId
+// The socket identifier.
+//
+// string	peerAddress
+// The address of the remote machine. DNS name, IPv4 and IPv6 formats are supported.
+//
+// integer	peerPort
+// The port of the remote machine.
+//
+// function	callback
+// Called when the connect attempt is complete.
+//
+// The callback parameter should be a function that looks like this:
+//
+// function(integer result) {...};
+// integer	result
+// The result code returned from the underlying network call. A negative value indicates an error.
+func Connect(socketId int, peerAddress string, peerPort int, callback func(result int)) {
+	tcp.Call("connect", socketId, peerAddress, peerPort, callback)
+}
+
+// secure
+//
+// chrome.sockets.tcp.secure(integer socketId, object options, function callback)
+// Since Chrome 38.
+//
+// Start a TLS client connection over the connected TCP client socket.
+//
+// Parameters
+// integer	socketId
+// The existing, connected socket to use.
+//
+// object	(optional) options
+// Constraints and parameters for the TLS connection.
+//
+// object	(optional) tlsVersion
+// string	(optional) min
+// The minimum and maximum acceptable versions of TLS. These will be tls1, tls1.1, or tls1.2.
+//
+// string	(optional) max
+// function	callback
+// Called when the connection attempt is complete.
+//
+// The callback parameter should be a function that looks like this:
+//
+// function(integer result) {...};
+// integer	result
+func Secure(socketId int, callback func(result int)) {
+	tcp.Call("secure", js.M{
+		"tlsVersion": js.M{
+			"min": "tsl1",
+			"max": "tsl.2",
+		},
+	}, callback)
+}
+
+type SendInfo struct {
+	*js.Object
+	// integer	resultCode
+	// The result code returned from the underlying network call. A negative value indicates an error.
+	ResultCode int `js:"resultCode"`
+	// integer	(optional) bytesSent
+	// The number of bytes sent (if result == 0)
+	BytesSent int `js:"bytesSent"`
+}
+
+// send
+//
+// chrome.sockets.tcp.send(integer socketId, ArrayBuffer data, function callback)
+// Sends data on the given TCP socket.
+//
+// Parameters
+// integer	socketId
+// The socket identifier.
+//
+// ArrayBuffer	data
+// The data to send.
+//
+// function	callback
+// Called when the send operation completes.
+//
+// The callback parameter should be a function that looks like this:
+//
+// function(object sendInfo) {...};
+// object	sendInfo
+// Result of the send method.
+//
+// integer	resultCode
+// The result code returned from the underlying network call. A negative value indicates an error.
+//
+// integer	(optional) bytesSent
+// The number of bytes sent (if result == 0)
+func Send(socketId int, data []byte, callback func(*SendInfo)) {
+	tcp.Call("send", socketId, js.NewArrayBuffer(data), callback)
+}
+
+type ReceiveInfo struct {
+	*js.Object
+	// integer	socketId
+	// The socket identifier.
+	SocketId int `js:"socketId"`
+	// ArrayBuffer	data
+	// The data received, with a maxium size of bufferSize.
+	Data []byte `js:"data"`
+}
+
+// onReceive
+//
+// Event raised when data has been received for a given socket.
+//
+// addListener
+// chrome.sockets.tcp.onReceive.addListener(function callback)
+// Parameters
+// function	callback
+// The callback parameter should be a function that looks like this:
+//
+// function(object info) {...};
+// object	info
+// The event data.
+//
+// integer	socketId
+// The socket identifier.
+//
+// ArrayBuffer	data
+// The data received, with a maxium size of bufferSize.
+func OnReceive(callback func(*ReceiveInfo)) {
+	tcp.Get("onReceive").Call("addListener", callback)
+}
+
+type ReceiveError struct {
+	*js.Object
+	// integer	socketId
+	// The socket identifier.
+	SocketId int `js:"socketId"`
+	// integer	resultCode
+	// The result code returned from the underlying network call.
+	ResultCode int `js:"resultCode"`
+}
+
+// onReceiveError
+//
+// Event raised when a network error occured while the runtime was waiting for data on the socket address and port. Once this event is raised, the socket is set to paused and no more onReceive events are raised for this socket.
+//
+// addListener
+// chrome.sockets.tcp.onReceiveError.addListener(function callback)
+// Parameters
+// function	callback
+// The callback parameter should be a function that looks like this:
+//
+// function(object info) {...};
+// object	info
+// The event data.
+//
+// integer	socketId
+// The socket identifier.
+//
+// integer	resultCode
+// The result code returned from the underlying network call.
+func OnReceiveError(callback func(*ReceiveError)) {
+	tcp.Get("onReceiveError").Call("addListener", callback)
+}
+
+// setKeepAlive
+//
+// chrome.sockets.tcp.setKeepAlive(integer socketId, boolean enable, integer delay, function callback)
+// Enables or disables the keep-alive functionality for a TCP connection.
+//
+// Parameters
+// integer	socketId
+// The socket identifier.
+//
+// boolean	enable
+// If true, enable keep-alive functionality.
+//
+// integer	(optional) delay
+// Set the delay seconds between the last data packet received and the first keepalive probe. Default is 0.
+//
+// function	callback
+// Called when the setKeepAlive attempt is complete.
+//
+// The callback parameter should be a function that looks like this:
+//
+// function(integer result) {...};
+// integer	result
+// The result code returned from the underlying network call. A negative value indicates an error.
+func SetKeepAlive(socketId int, enable bool, delay int, callback func(result int)) {
+	tcp.Call("setKeepAlive", socketId, enable, delay, callback)
+}
+
+// setNoDelay
+//
+// chrome.sockets.tcp.setNoDelay(integer socketId, boolean noDelay, function callback)
+// Sets or clears TCP_NODELAY for a TCP connection. Nagle's algorithm will be disabled when TCP_NODELAY is set.
+//
+// Parameters
+// integer	socketId
+// The socket identifier.
+//
+// boolean	noDelay
+// If true, disables Nagle's algorithm.
+//
+// function	callback
+// Called when the setNoDelay attempt is complete.
+//
+// The callback parameter should be a function that looks like this:
+//
+// function(integer result) {...};
+// integer	result
+// The result code returned from the underlying network call. A negative value indicates an error.
+func SetNoDelay(socketId int, noDelay bool, callback func(result int)) {
+	tcp.Call("setNoDelay", socketId, noDelay, callback)
+}
+
+// disconnect
+//
+// chrome.sockets.tcp.disconnect(integer socketId, function callback)
+// Disconnects the socket.
+//
+// Parameters
+// integer	socketId
+// The socket identifier.
+//
+// function	(optional) callback
+// Called when the disconnect attempt is complete.
+//
+// If you specify the callback parameter, it should be a function that looks like this:
+//
+// function() {...};
+func Disconnect(socketId int) {
+	tcp.Call("disconnect", socketId)
+}
+
+// close
+
+// chrome.sockets.tcp.close(integer socketId, function callback)
+// Closes the socket and releases the address/port the socket is bound to.
+// Each socket created should be closed after use.
+// The socket id is no no longer valid as soon at the function is called.
+// However, the socket is guaranteed to be closed only when the callback is invoked.
+//
+// Parameters
+// integer	socketId
+// The socket identifier.
+//
+// function	(optional) callback
+// Called when the close operation completes.
+//
+// If you specify the callback parameter, it should be a function that looks like this:
+//
+// function() {...};
+func Close(socketId int) {
+	tcp.Call("close", socketId)
+}
+
+// getInfo
+//
+// chrome.sockets.tcp.getInfo(integer socketId, function callback)
+// Retrieves the state of the given socket.
+//
+// Parameters
+// integer	socketId
+// The socket identifier.
+//
+// function	callback
+// Called when the socket state is available.
+//
+// The callback parameter should be a function that looks like this:
+//
+// function( SocketInfo socketInfo) {...};
+// SocketInfo	socketInfo
+// Object containing the socket information.
+func GetInfo(socketId int, callback func(*sockets.SocketInfo)) {
+	tcp.Call("getInfo", socketId, callback)
+}
+
+// getSockets
+//
+// chrome.sockets.tcp.getSockets(function callback)
+// Retrieves the list of currently opened sockets owned by the application.
+//
+// Parameters
+// function	callback
+// Called when the list of sockets is available.
+//
+// The callback parameter should be a function that looks like this:
+//
+// function(array of SocketInfo socketInfos) {...};
+// array of SocketInfo	socketInfos
+// Array of object containing socket information.
+func GetSockets(callback func([]*sockets.SocketInfo)) {
+	tcp.Call("getSockets", callback)
 }
