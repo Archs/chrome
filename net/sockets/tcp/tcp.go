3ceda925e6dd0c24e9a65054f2833ae8d8475e85
Archs <gitarchs@yeah.net>
2015-05-19 11:46:01 +0800

    init net support for chrome

---
+++ net/sockets/tcp/tcp.go
@@ -0,0 +1,31 @@
+package net
+
+import (
+	"github.com/gopherjs/gopherjs/js"
+)
+
+var (
+	tcp = js.Global
+)
+
+// chrome.sockets.tcp.create( SocketProperties properties, function callback)
+// Creates a TCP socket.
+
+// Parameters
+// SocketProperties	(optional) properties
+// The socket properties (optional).
+
+// function	callback
+// Called when the socket has been created.
+
+// The callback parameter should be a function that looks like this:
+
+// function(object createInfo) {...};
+// object	createInfo
+// The result of the socket creation.
+
+// integer	socketId
+// The ID of the newly created socket. Note that socket IDs created from this API are not compatible with socket IDs created from other APIs, such as the deprecated socket API.
+func (t *Tcp) Create(p SocketProperties, callback func(CreateInfo)) {
+	t.Object.Call("create", p, callback)
+}
---
+++ net/sockets/udp/udp.go
@@ -0,0 +1,9 @@
+package udp
+
+import (
+	"github.com/Archs/chrome/net/sockets"
+)
+
+var (
+	udp = sockets.Get("udp")
+)
