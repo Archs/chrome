9580a1782a225e2976e46f08b228043c6a34d988
Archs <sun@archs.tk>
2015-05-20 07:34:41 +0800

    refine chrome.bookmarks

--- api/bookmarks/bookmarks.go
+++ api/bookmarks/bookmarks.go
@@ -16,15 +16,15 @@ var (
 
 type TreeNode struct {
 	*js.Object
-	Id                string     `js:"id"`
-	ParentId          string     `js:"parentId"`
-	Index             int        `js:"index"`
-	Url               string     `js:"url"`
-	Title             string     `js:"title"`
-	DateAdded         int64      `js:"dateAdded"`
-	DateGroupModified int64      `js:"dateGroupModified"`
-	Unmodifiable      string     `js:"unmodifiable"`
-	Children          []TreeNode `js:"children"`
+	Id                string      `js:"id"`
+	ParentId          string      `js:"parentId"`
+	Index             int         `js:"index"`
+	Url               string      `js:"url"`
+	Title             string      `js:"title"`
+	DateAdded         int64       `js:"dateAdded"`
+	DateGroupModified int64       `js:"dateGroupModified"`
+	Unmodifiable      string      `js:"unmodifiable"`
+	Children          []*TreeNode `js:"children"`
 }
 
 /*
@@ -32,50 +32,50 @@ type TreeNode struct {
  */
 
 // Get retrieves the specified TreeNode(s).
-func Get(idList []string, callback func(results []TreeNode)) {
+func Get(idList []string, callback func(results []*TreeNode)) {
 	bookmarks.Call("get", idList, callback)
 }
 
 // GetChildren retrieves the children of the specified TreeNode id.
-func GetChildren(id string, callback func(results []TreeNode)) {
+func GetChildren(id string, callback func(results []*TreeNode)) {
 	bookmarks.Call("getChildren", id, callback)
 }
 
 // GetRecent retrieves the recently added bookmarks.
-func GetRecent(numberOfItems int, callback func(results []TreeNode)) {
+func GetRecent(numberOfItems int, callback func(results []*TreeNode)) {
 	bookmarks.Call("getRecent", numberOfItems, callback)
 }
 
 // GetTree retrieves the entire Bookmarks hierarchy.
-func GetTree(callback func(results []TreeNode)) {
+func GetTree(callback func(results []*TreeNode)) {
 	bookmarks.Call("getTree", callback)
 }
 
 // GetSubTree retrieves part of the Bookmarks hierarchy, starting at the specified node.
-func GetSubTree(id string, callback func(results []TreeNode)) {
+func GetSubTree(id string, callback func(results []*TreeNode)) {
 	bookmarks.Call("getSubTree", id, callback)
 }
 
 // Search searches for TreeNodes matching the given query. Queries specified
 // with an js.M produce TreeNodes matching all specified properties.
-func Search(query interface{}, callback func(results []TreeNode)) {
+func Search(query interface{}, callback func(results []*TreeNode)) {
 	bookmarks.Call("search", query, callback)
 }
 
 // Create creates a bookmark or folder under the specified parentId.
 // If url is nil or missing, it will be a folder.
-func Create(bookmark js.M, callback func(result TreeNode)) {
+func Create(bookmark js.M, callback func(result *TreeNode)) {
 	bookmarks.Call("create", bookmark, callback)
 }
 
 // Move moves the specified TreeNode to the provided location.
-func Move(id string, destination js.M, callback func(result TreeNode)) {
+func Move(id string, destination js.M, callback func(result *TreeNode)) {
 	bookmarks.Call("move", id, destination, callback)
 }
 
 // Update updates the properties of a bookmark or folder. Specify only the properties that you want
 // to change; unspecified properties will be left unchanged. Note: Currently, only 'title' and 'url' are supported.
-func Update(id string, changes js.M, callback func(result TreeNode)) {
+func Update(id string, changes js.M, callback func(result *TreeNode)) {
 	bookmarks.Call("update", id, changes, callback)
 }
 
@@ -94,7 +94,7 @@ func RemoveTree(id string, callback func()) {
  */
 
 // OnCreated fired when a bookmark or folder is created.
-func OnCreated(callback func(id string, bookmark TreeNode)) {
+func OnCreated(callback func(id string, bookmark *TreeNode)) {
 	bookmarks.Get("onCreated").Call("addListener", callback)
 }
 
