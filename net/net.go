c277fdc501741a8f4742c6faa2fe4de6c12e199b
Archs <sun@archs.tk>
2015-05-19 23:37:43 +0800

    seperate tcpConn into different file

--- net/net.go
+++ net/net.go
@@ -8,16 +8,7 @@ import (
 	"github.com/Archs/chrome/net/sockets/tcp"
 	"github.com/Archs/chrome/net/sockets/tcpserver"
 	"net"
-	"sync"
 	"time"
-)
-
-var (
-	// scoket map
-	tcpMap = make(map[int]*tcpConn)
-	m      = new(sync.Mutex)
-	// listen chan
-	listenerMap = make(map[int]*tcpListener)
 )
 
 type Conn net.Conn
@@ -35,28 +26,6 @@ func newBuffer() *buffer {
 		readError:    nil,
 		readDeadLine: time.Time{},
 	}
-}
-
-type tcpConn struct {
-	*buffer
-	socketId int
-}
-
-func newTcpConn(socketId int) *tcpConn {
-	// println("creating conn, id:", socketId)
-	conn := &tcpConn{
-		socketId: socketId,
-		buffer:   newBuffer(),
-	}
-	tcpMap[socketId] = conn
-	// println("c tcpMap length:", len(tcpMap))
-	return conn
-}
-
-type tcpListener struct {
-	socketId int
-	ch       chan int
-	err      error
 }
 
 func Dial(network, address string) (net.Conn, error) {
@@ -113,43 +82,6 @@ func Listen(network, laddr string) (net.Listener, error) {
 	return l, nil
 }
 
-// Accept waits for and returns the next connection to the listener.
-func (cl *tcpListener) Accept() (c net.Conn, err error) {
-	if cl.err != nil {
-		c = nil
-		err = cl.err
-		cl.err = nil
-		return
-	}
-	id := <-cl.ch
-	// println("listener.Accept socket id", id)
-	c = newTcpConn(id)
-	return
-}
-
-// Close closes the listener.
-// Any blocked Accept operations will be unblocked and return errors.
-func (c *tcpListener) Close() error {
-	tcpserver.Close(c.socketId)
-	delete(listenerMap, c.socketId)
-	return nil
-}
-
-// Addr returns the listener's network address.
-func (c *tcpListener) Addr() net.Addr {
-	var addr net.Addr
-	ch := make(chan struct{})
-	tcpserver.GetInfo(c.socketId, func(si *sockets.SocketInfo) {
-		addr = &net.TCPAddr{
-			IP:   net.ParseIP(si.LocalAddress),
-			Port: si.LocalPort,
-		}
-		close(ch)
-	})
-	<-ch
-	return addr
-}
-
 type timeoutError struct {
 	net.OpError
 	isTimeOut bool
@@ -197,62 +129,6 @@ func (c *buffer) Read(b []byte) (n int, err error) {
 	}
 }
 
-// Write writes data to the connection.
-// Write can be made to time out and return a Error with Timeout() == true
-// after a fixed time limit; see SetDeadline and SetWriteDeadline.
-func (c *tcpConn) Write(b []byte) (n int, err error) {
-	ch := make(chan struct{})
-	tcp.Send(c.socketId, b, func(si *tcp.SendInfo) {
-		if si.ResultCode < 0 {
-			err = fmt.Errorf("socket write failed: %d", si.ResultCode)
-		} else {
-			n = si.BytesSent
-		}
-		close(ch)
-	})
-	<-ch
-	return
-}
-
-// Close closes the connection.
-// Any blocked Read or Write operations will be unblocked and return errors.
-func (c *tcpConn) Close() error {
-	// println("conn.Close called")
-	tcp.Close(c.socketId)
-	delete(tcpMap, c.socketId)
-	return nil
-}
-
-// LocalAddr returns the local network address.
-func (c *tcpConn) LocalAddr() net.Addr {
-	var addr net.Addr
-	ch := make(chan struct{})
-	tcp.GetInfo(c.socketId, func(si *sockets.SocketInfo) {
-		addr = &net.TCPAddr{
-			IP:   net.ParseIP(si.LocalAddress),
-			Port: si.LocalPort,
-		}
-		close(ch)
-	})
-	<-ch
-	return addr
-}
-
-// RemoteAddr returns the remote network address.
-func (c *tcpConn) RemoteAddr() net.Addr {
-	var addr net.Addr
-	ch := make(chan struct{})
-	tcp.GetInfo(c.socketId, func(si *sockets.SocketInfo) {
-		addr = &net.TCPAddr{
-			IP:   net.ParseIP(si.PeerAddress),
-			Port: si.PeerPort,
-		}
-		close(ch)
-	})
-	<-ch
-	return addr
-}
-
 // SetDeadline sets the read and write deadlines associated
 // with the connection. It is equivalent to calling both
 // SetReadDeadline and SetWriteDeadline.
@@ -285,46 +161,4 @@ func (c *buffer) SetReadDeadline(t time.Time) error {
 // A zero value for t means Write will not time out.
 func (c *buffer) SetWriteDeadline(t time.Time) error {
 	return nil
-}
-
-func init() {
-	tcp.OnReceive(func(ri *tcp.ReceiveInfo) {
-		go func() {
-			m.Lock()
-			defer m.Unlock()
-			// println("tcp receive on socket:", ri.SocketId)
-			// println("tcpMap length:", len(tcpMap))
-			conn, ok := tcpMap[ri.SocketId]
-			if ok {
-				// println("ri.Data", ri.Data)
-				conn.readBuf.Write(ri.Data)
-			} else {
-				println("no conn found", tcpMap)
-			}
-		}()
-	})
-	tcp.OnReceiveError(func(re *tcp.ReceiveError) {
-		m.Lock()
-		defer m.Unlock()
-		conn, ok := tcpMap[re.SocketId]
-		if ok {
-			conn.readError = fmt.Errorf("recv error code %d", re.ResultCode)
-		}
-	})
-	tcpserver.OnAccept(func(ai *tcpserver.AcceptInfo) {
-		// println("tcpserver accept:", ai.ClientSocketId)
-		cl, ok := listenerMap[ai.SocketId]
-		if !ok {
-			return
-		}
-		tcp.SetPaused(ai.ClientSocketId, false)
-		cl.ch <- ai.ClientSocketId
-	})
-	tcpserver.OnAcceptError(func(ae *tcpserver.AcceptError) {
-		cl, ok := listenerMap[ae.SocketId]
-		if !ok {
-			return
-		}
-		cl.err = fmt.Errorf("accept error: %d", ae.ResultCode)
-	})
 }
---
+++ net/tcp.go
@@ -0,0 +1,175 @@
+package net
+
+import (
+	"fmt"
+	"github.com/Archs/chrome/net/sockets"
+	"github.com/Archs/chrome/net/sockets/tcp"
+	"github.com/Archs/chrome/net/sockets/tcpserver"
+	"net"
+	"sync"
+)
+
+var (
+	// scoket map
+	tcpMap = make(map[int]*tcpConn)
+	m      = new(sync.Mutex)
+	// listen chan
+	listenerMap = make(map[int]*tcpListener)
+)
+
+type tcpConn struct {
+	*buffer
+	socketId int
+}
+
+func newTcpConn(socketId int) *tcpConn {
+	// println("creating conn, id:", socketId)
+	conn := &tcpConn{
+		socketId: socketId,
+		buffer:   newBuffer(),
+	}
+	tcpMap[socketId] = conn
+	// println("c tcpMap length:", len(tcpMap))
+	return conn
+}
+
+type tcpListener struct {
+	socketId int
+	ch       chan int
+	err      error
+}
+
+// Accept waits for and returns the next connection to the listener.
+func (cl *tcpListener) Accept() (c net.Conn, err error) {
+	if cl.err != nil {
+		c = nil
+		err = cl.err
+		cl.err = nil
+		return
+	}
+	id := <-cl.ch
+	// println("listener.Accept socket id", id)
+	c = newTcpConn(id)
+	return
+}
+
+// Close closes the listener.
+// Any blocked Accept operations will be unblocked and return errors.
+func (c *tcpListener) Close() error {
+	tcpserver.Close(c.socketId)
+	delete(listenerMap, c.socketId)
+	return nil
+}
+
+// Addr returns the listener's network address.
+func (c *tcpListener) Addr() net.Addr {
+	var addr net.Addr
+	ch := make(chan struct{})
+	tcpserver.GetInfo(c.socketId, func(si *sockets.SocketInfo) {
+		addr = &net.TCPAddr{
+			IP:   net.ParseIP(si.LocalAddress),
+			Port: si.LocalPort,
+		}
+		close(ch)
+	})
+	<-ch
+	return addr
+}
+
+// Write writes data to the connection.
+// Write can be made to time out and return a Error with Timeout() == true
+// after a fixed time limit; see SetDeadline and SetWriteDeadline.
+func (c *tcpConn) Write(b []byte) (n int, err error) {
+	ch := make(chan struct{})
+	tcp.Send(c.socketId, b, func(si *tcp.SendInfo) {
+		if si.ResultCode < 0 {
+			err = fmt.Errorf("socket write failed: %d", si.ResultCode)
+		} else {
+			n = si.BytesSent
+		}
+		close(ch)
+	})
+	<-ch
+	return
+}
+
+// Close closes the connection.
+// Any blocked Read or Write operations will be unblocked and return errors.
+func (c *tcpConn) Close() error {
+	// println("conn.Close called")
+	tcp.Close(c.socketId)
+	delete(tcpMap, c.socketId)
+	return nil
+}
+
+// LocalAddr returns the local network address.
+func (c *tcpConn) LocalAddr() net.Addr {
+	var addr net.Addr
+	ch := make(chan struct{})
+	tcp.GetInfo(c.socketId, func(si *sockets.SocketInfo) {
+		addr = &net.TCPAddr{
+			IP:   net.ParseIP(si.LocalAddress),
+			Port: si.LocalPort,
+		}
+		close(ch)
+	})
+	<-ch
+	return addr
+}
+
+// RemoteAddr returns the remote network address.
+func (c *tcpConn) RemoteAddr() net.Addr {
+	var addr net.Addr
+	ch := make(chan struct{})
+	tcp.GetInfo(c.socketId, func(si *sockets.SocketInfo) {
+		addr = &net.TCPAddr{
+			IP:   net.ParseIP(si.PeerAddress),
+			Port: si.PeerPort,
+		}
+		close(ch)
+	})
+	<-ch
+	return addr
+}
+
+func init() {
+	tcp.OnReceive(func(ri *tcp.ReceiveInfo) {
+		go func() {
+			m.Lock()
+			defer m.Unlock()
+			// println("tcp receive on socket:", ri.SocketId)
+			// println("tcpMap length:", len(tcpMap))
+			conn, ok := tcpMap[ri.SocketId]
+			if ok {
+				// println("ri.Data", ri.Data)
+				conn.readBuf.Write(ri.Data)
+			} else {
+				println("no conn found", tcpMap)
+			}
+		}()
+	})
+	tcp.OnReceiveError(func(re *tcp.ReceiveError) {
+		m.Lock()
+		defer m.Unlock()
+		conn, ok := tcpMap[re.SocketId]
+		if ok {
+			conn.readError = fmt.Errorf("recv error code %d", re.ResultCode)
+		}
+	})
+	tcpserver.OnAccept(func(ai *tcpserver.AcceptInfo) {
+		// println("tcpserver accept:", ai.ClientSocketId)
+		cl, ok := listenerMap[ai.SocketId]
+		if !ok {
+			return
+		}
+		tcp.SetPaused(ai.ClientSocketId, false)
+		cl.ch <- ai.ClientSocketId
+	})
+	tcpserver.OnAcceptError(func(ae *tcpserver.AcceptError) {
+		cl, ok := listenerMap[ae.SocketId]
+		if !ok {
+			return
+		}
+		cl.err = fmt.Errorf("accept error: %d", ae.ResultCode)
+	})
+}
