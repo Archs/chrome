2e5e384dfe033f289fa0426f465aaf488813ddd4
Archs <sun@archs.tk>
2015-05-20 06:26:48 +0800

    knockout.mapping works

--- examples/bookmarks/main.go
+++ examples/bookmarks/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"github.com/Archs/chrome/api/bookmarks"
-	"github.com/Archs/gopherjs-ko"
-	"github.com/Archs/js/JSON"
-	"github.com/gopherjs/gopherjs/js"
+	// "github.com/Archs/js/JSON"
+	"github.com/Archs/js/gopherjs-ko"
+	// "github.com/gopherjs/gopherjs/js"
 )
 
 var (
@@ -14,15 +14,16 @@ var (
 
 func main() {
 	ko.EnableSecureBinding()
-	model := js.M{
-		"out":       out,
-		"bookmarks": bmks,
-	}
-	ko.ApplyBindings(model)
+	// model := js.M{
+	// 	"out":       out,
+	// 	"bookmarks": bmks,
+	// }
 	bookmarks.GetTree(func(bs []bookmarks.TreeNode) {
-		bmks.Set(bs)
-		str := JSON.Stringify(bs)
-		println(str)
-		out.Set(str)
+		bs[0].Title = "Root"
+		vm := ko.Mapping().FromJS(bs[0])
+		vm.Call("title", "title change after mapping")
+		println("vm:", vm)
+		println("vm.toJSON:", ko.Mapping().ToJSON(vm))
+		ko.ApplyBindings(vm)
 	})
 }
