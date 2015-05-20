c613e48b9cc861320858e20116e75f4eb2544296
Archs <gitarchs@yeah.net>
2015-05-20 10:36:09 +0800

    using dom.OnDOMContentLoaded

--- examples/bookmarks/main.go
+++ examples/bookmarks/main.go
@@ -2,20 +2,25 @@ package main
 
 import (
 	"github.com/Archs/chrome/api/bookmarks"
+	"github.com/Archs/js/dom"
 	"github.com/Archs/js/gopherjs-ko"
+	"github.com/Archs/js/utils/property"
 	"github.com/gopherjs/gopherjs/js"
 )
 
 func main() {
+	dom.OnDOMContentLoaded(run)
+}
+
+func run() {
 	ko.EnableSecureBinding()
 	println("Get:", ko.Global().Get("asdfasdfafd") == js.Undefined)
 	bookmarks.GetTree(func(bs []*bookmarks.TreeNode) {
 		bs[0].Title = "Root"
 		vm := ko.Mapping().FromJS(bs[0])
-		// vm.Set2("TT", "title change after mapping")
 		println("vm.toJSON:", ko.Mapping().ToJSON(vm))
 		println("vm.title:", vm.Get("title"))
-		vm.Set("tt", 123)
+		println("vm.children:", vm.Get("title"))
 		ko.ApplyBindings(vm)
 	})
 }
