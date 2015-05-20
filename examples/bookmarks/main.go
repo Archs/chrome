e01ecaca07801513794f8e797bb77001d4865740
Archs <sun@archs.tk>
2015-05-20 12:28:33 +0800

    update

--- examples/bookmarks/main.go
+++ examples/bookmarks/main.go
@@ -1,26 +1,70 @@
 package main
 
 import (
-	"github.com/Archs/chrome/api/bookmarks"
+	// "github.com/Archs/chrome/api/bookmarks"
 	"github.com/Archs/js/dom"
 	"github.com/Archs/js/gopherjs-ko"
-	"github.com/Archs/js/utils/property"
 	"github.com/gopherjs/gopherjs/js"
+	"time"
 )
 
 func main() {
 	dom.OnDOMContentLoaded(run)
 }
 
+type MainController struct {
+	*js.Object
+	Title    ko.Observable       `js:"Title"`
+	Bookmark *BookmarkController `js:"Bookmark"`
+}
+
+type BookmarkController struct {
+	*js.Object
+	// Title ko.Observable       `js:"Title"`
+	Show     func()             `js:"Show"`
+	TextArea ko.Observable      `js:"TextArea"`
+	Time     ko.Observable      `js:"Time"`
+	Root     ko.MappedViewModel `js:"Root"`
+	// Str      string              `js:"Str"`
+	Str string
+}
+
+func newBkmkCtrl() *BookmarkController {
+	b := BookmarkController{}
+	b.Object = js.Global.Get("Object").New()
+	b.TextArea = ko.NewObservable("")
+	b.Time = ko.NewObservable(time.Now().String())
+	b.Str = "nice to see you"
+	b.Show = func() {
+		b.Time.Set(time.Now().String())
+		// b.TextArea.Set(ko.Mapping().ToJSON(b))
+		b.TextArea.Set(b.Time.Get().String())
+		println(b, b.Str)
+	}
+	return &b
+}
+
+func newMainCtrl() MainController {
+	m := MainController{}
+	m.Object = js.Global.Get("Object").New()
+	m.Title = ko.NewObservable("Bookmark Manager")
+	m.Bookmark = newBkmkCtrl()
+	return m
+}
+
 func run() {
 	ko.EnableSecureBinding()
+	// model := newBkmkCtrl()
+	// println("model:", model)
+	// ko.ApplyBindings(model)
 	println("Get:", ko.Global().Get("asdfasdfafd") == js.Undefined)
-	bookmarks.GetTree(func(bs []*bookmarks.TreeNode) {
-		bs[0].Title = "Root"
-		vm := ko.Mapping().FromJS(bs[0])
-		println("vm.toJSON:", ko.Mapping().ToJSON(vm))
-		println("vm.title:", vm.Get("title"))
-		println("vm.children:", vm.Get("title"))
-		ko.ApplyBindings(vm)
-	})
+	model := newMainCtrl()
+	println(model.Bookmark.Show)
+	sh := model.Bookmark.Show
+	sh()
+	// bookmarks.GetTree(func(bs []*bookmarks.TreeNode) {
+	// 	bs[0].Title = "Root"
+	// 	model.Bookmark.Root = ko.Mapping().FromJS(bs[0])
+	// })
+	ko.ApplyBindings(model)
 }
