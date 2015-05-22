1d8722215627ebe5d1a2e8cd1173d146ee5b4626
Archs <gitarchs@yeah.net>
2015-05-23 02:23:45 +0800

    seperate webRequest

--- webRequest/web_request.go
+++ webRequest/web_request.go
@@ -1,20 +1,14 @@
-package chrome
+package webRequest
 
-import "github.com/gopherjs/gopherjs/js"
+import (
+	"github.com/Archs/chrome"
+	"github.com/gopherjs/gopherjs/js"
+)
 
-type WebRequest struct {
-	o                                                 *js.Object
-	MAX_HANDLER_BEHAVIOR_CHANGED_CALLS_PER_10_MINUTES int
-}
-
-func NewWebRequest(webRequestObj *js.Object) *WebRequest {
-	w := new(WebRequest)
-	w.o = webRequestObj
-	if w.o.String() != "undefined" {
-		w.MAX_HANDLER_BEHAVIOR_CHANGED_CALLS_PER_10_MINUTES = w.o.Get("MAX_HANDLER_BEHAVIOR_CHANGED_CALLS_PER_10_MINUTES").Int()
-	}
-	return w
-}
+var (
+	webRequest                                        = chrome.Get("webRequest")
+	MAX_HANDLER_BEHAVIOR_CHANGED_CALLS_PER_10_MINUTES = webRequest.Get("MAX_HANDLER_BEHAVIOR_CHANGED_CALLS_PER_10_MINUTES").Int()
+)
 
 /*
 * Types
@@ -49,8 +43,8 @@ type UploadData struct {
 
 // HandlerBehaviorChanged needs to be called when the behavior of the webRequest handlers has
 // changed to prevent incorrect handling due to caching. This function call is expensive. Don't call it often.
-func (w *WebRequest) HandlerBehaviorChanged(callback func()) {
-	w.o.Call("handlerBehaviorChanged", callback)
+func HandlerBehaviorChanged(callback func()) {
+	webRequest.Call("handlerBehaviorChanged", callback)
 }
 
 /*
@@ -58,52 +52,52 @@ func (w *WebRequest) HandlerBehaviorChanged(callback func()) {
  */
 
 // OnBeforeRequest fired when a request is about to occur.
-func (w *WebRequest) OnBeforeRequest(callback func(details js.M)) {
-	w.o.Get("onBeforeRequest").Call("addListener", callback)
+func OnBeforeRequest(callback func(details js.M)) {
+	webRequest.Get("onBeforeRequest").Call("addListener", callback)
 }
 
 // OnBeforeSendHeaders fired before sending an HTTP request, once the request headers are available.
 // This may occur after a TCP connection is made to the server, but before any HTTP data is sent.
-func (w *WebRequest) OnBeforeSendHeaders(callback func(details js.M)) {
-	w.o.Get("onBeforeSendHeaders").Call("addListener", callback)
+func OnBeforeSendHeaders(callback func(details js.M)) {
+	webRequest.Get("onBeforeSendHeaders").Call("addListener", callback)
 }
 
 // OnSendHeaders fired just before a request is going to be sent to the server (modifications of
 // previous onBeforeSendHeaders callbacks are visible by the time onSendHeaders is fired).
-func (w *WebRequest) OnSendHeaders(callback func(details js.M)) {
-	w.o.Get("onSendHeaders").Call("addListener", callback)
+func OnSendHeaders(callback func(details js.M)) {
+	webRequest.Get("onSendHeaders").Call("addListener", callback)
 }
 
 // OnHeadersReceived fired when HTTP response headers of a request have been received.
-func (w *WebRequest) OnHeadersReceived(callback func(details js.M)) {
-	w.o.Get("onHeadersReceived").Call("addListener", callback)
+func OnHeadersReceived(callback func(details js.M)) {
+	webRequest.Get("onHeadersReceived").Call("addListener", callback)
 }
 
 // OnAuthRequired fired when an authentication failure is received. The listener has three options:
 // it can provide authentication credentials, it can cancel the request and display the error page,
 // or it can take no action on the challenge. If bad user credentials are provided, this may be
 // called multiple times for the same request.
-func (w *WebRequest) OnAuthRequired(callback func(details js.M)) {
-	w.o.Get("onAuthRequired").Call("addListener", callback)
+func OnAuthRequired(callback func(details js.M)) {
+	webRequest.Get("onAuthRequired").Call("addListener", callback)
 }
 
 // OnResponseStarted fired when the first byte of the response body is received. For HTTP requests,
 // this means that the status line and response headers are available.
-func (w *WebRequest) OnResponseStarted(callback func(details js.M)) {
-	w.o.Get("onResponseStarted").Call("addListener", callback)
+func OnResponseStarted(callback func(details js.M)) {
+	webRequest.Get("onResponseStarted").Call("addListener", callback)
 }
 
 // OnBeforeRedirect fired when a server-initiated redirect is about to occur.
-func (w *WebRequest) OnBeforeRedirect(callback func(details js.M)) {
-	w.o.Get("onBeforeRedirect").Call("addListener", callback)
+func OnBeforeRedirect(callback func(details js.M)) {
+	webRequest.Get("onBeforeRedirect").Call("addListener", callback)
 }
 
 // OnCompleted fired when a request is completed.
-func (w *WebRequest) OnCompleted(callback func(details js.M)) {
-	w.o.Get("onCompleted").Call("addListener", callback)
+func OnCompleted(callback func(details js.M)) {
+	webRequest.Get("onCompleted").Call("addListener", callback)
 }
 
 // OnErrorOccured fired when an error occurs.
-func (w *WebRequest) OnErrorOccured(callback func(details js.M)) {
-	w.o.Get("onErrorOccured").Call("addListener", callback)
+func OnErrorOccured(callback func(details js.M)) {
+	webRequest.Get("onErrorOccured").Call("addListener", callback)
 }
