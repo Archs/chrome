f36d71deff2579a8cb58f52b5f83b4466544b342
Archs <sun@archs.tk>
2015-05-23 01:27:39 +0800

    seperate fileBrowserHandler

--- fileSystemProvider/file-system-provider.go
+++ fileSystemProvider/file-system-provider.go
@@ -1,16 +1,17 @@
 // WARNING: This API only works on Chrome OS
 
-package chrome
+package fileSystemProvider
 
 import (
+	"github.com/Archs/chrome"
 	"time"
 
 	"github.com/gopherjs/gopherjs/js"
 )
 
-type FileSystemProvider struct {
-	o *js.Object
-}
+var (
+	fileSystemProvider = chrome.Get("fileSystemProvider")
+)
 
 /*
 * Types
@@ -35,11 +36,11 @@ type Watcher struct {
 
 type FileSystemInfo struct {
 	*js.Object
-	FileSystemId    string   `js:"fileSystemId"`
-	DisplayName     string   `js:"displayName"`
-	Writable        bool     `js:"writable"`
-	OpenedFileLimit int64    `js:"openedFileLimit"`
-	OpenedFiles     []Object `js:"openedFiles"`
+	FileSystemId    string `js:"fileSystemId"`
+	DisplayName     string `js:"displayName"`
+	Writable        bool   `js:"writable"`
+	OpenedFileLimit int64  `js:"openedFileLimit"`
+	OpenedFiles     []js.M `js:"openedFiles"`
 }
 
 type AddWatcherRequestedOptions struct {
@@ -54,12 +55,12 @@ type RemoveWatcherRequestedOptions AddWatcherRequestedOptions
 
 type NotifyOptions struct {
 	*js.Object
-	FilesystemId string   `js:"fileSystemId"`
-	ObservedPath string   `js:"observedPath"`
-	Recursive    bool     `js:"recursive"`
-	ChangeType   string   `js:"changeType"`
-	Changes      []Object `js:"changes"`
-	Tag          string   `js:"tag"`
+	FilesystemId string `js:"fileSystemId"`
+	ObservedPath string `js:"observedPath"`
+	Recursive    bool   `js:"recursive"`
+	ChangeType   string `js:"changeType"`
+	Changes      []js.M `js:"changes"`
+	Tag          string `js:"tag"`
 }
 
 /*
@@ -67,24 +68,24 @@ type NotifyOptions struct {
  */
 
 /* Mount mounts a file system with the given fileSystemId and displayName. displayName will be shown in the left panel of Files.app. displayName can contain any characters including '/', but cannot be an empty string. displayName must be descriptive but doesn't have to be unique. The fileSystemId must not be an empty string. */
-func (f *FileSystemProvider) Mount(options Object, callback func()) {
-	f.o.Call("mount", options, callback)
+func Mount(options js.M, callback func()) {
+	fileSystemProvider.Call("mount", options, callback)
 }
 
 // Unmount unmounts a file system with the given fileSystemId. It must be called after onUnmountRequested is invoked.
 // Also, the providing extension can decide to perform unmounting if not requested (eg. in case of lost connection, or a file error).
-func (f *FileSystemProvider) Unmount(options Object, callback func()) {
-	f.o.Call("unmount", options, callback)
+func Unmount(options js.M, callback func()) {
+	fileSystemProvider.Call("unmount", options, callback)
 }
 
 // GetAll returns all file systems mounted by the extension.
-func (f *FileSystemProvider) GetAll(callback func(fileSystems []FileSystemInfo)) {
-	f.o.Call("getAll", callback)
+func GetAll(callback func(fileSystems []FileSystemInfo)) {
+	fileSystemProvider.Call("getAll", callback)
 }
 
 // Get returns information about a file system with the passed fileSystemId.
-func (f *FileSystemProvider) Get(fileSystemId string, callback func(fileSystemInfo FileSystemInfo)) {
-	f.o.Call("get", fileSystemId, callback)
+func Get(fileSystemId string, callback func(fileSystemInfo FileSystemInfo)) {
+	fileSystemProvider.Call("get", fileSystemId, callback)
 }
 
 /*
@@ -94,81 +95,81 @@ func (f *FileSystemProvider) Get(fileSystemId string, callback func(fileSystemIn
 // OnUnmountRequested raised when unmounting for the file system with the fileSystemId identifier is requested.
 // In the response, the unmount API method must be called together with successCallback .
 // If unmounting is not possible (eg. due to a pending operation), then errorCallback must be called.
-func (f *FileSystemProvider) OnUnmountRequested(callback func(options Object, successCallback func(), errorCallback func(err string))) {
-	f.o.Get("onUnmountRequested").Call("addListener", callback)
+func OnUnmountRequested(callback func(options js.M, successCallback func(), errorCallback func(err string))) {
+	fileSystemProvider.Get("onUnmountRequested").Call("addListener", callback)
 }
 
 // OnGetMetadataRequested raised when metadata of a file or a directory at entryPath is requested.
 // The metadata must be returned with the successCallback call. In case of an error, errorCallback must be called.
-func (f *FileSystemProvider) OnGetMetadataRequested(callback func(options Object, successCallback func(metadata EntryMetadata), errorCallback func(err string))) {
-	f.o.Get("onGetMetadataRequested").Call("addListener", callback)
+func OnGetMetadataRequested(callback func(options js.M, successCallback func(metadata EntryMetadata), errorCallback func(err string))) {
+	fileSystemProvider.Get("onGetMetadataRequested").Call("addListener", callback)
 }
 
 // OnReadDirectoryRequested raised when contents of a directory at directoryPath are requested.
 // The results must be returned in chunks by calling the successCallback several times. In case of an error, errorCallback must be called.
-func (f *FileSystemProvider) OnReadDirectoryRequested(callback func(options Object, successCallback func(entries []EntryMetadata, hasMore bool), errorCallback func(err string))) {
-	f.o.Get("onReadDirectoryRequested").Call("addListener", callback)
+func OnReadDirectoryRequested(callback func(options js.M, successCallback func(entries []EntryMetadata, hasMore bool), errorCallback func(err string))) {
+	fileSystemProvider.Get("onReadDirectoryRequested").Call("addListener", callback)
 }
 
 // OnOpenFileRequested raised when opening a file at filePath is requested. If the file does not exist,
 // then the operation must fail. Maximum number of files opened at once can be specified with MountOptions.
-func (f *FileSystemProvider) OnOpenFileRequested(callback func(options Object, successCallback func(), errorCallback func(err string))) {
-	f.o.Get("onOpenFileRequested").Call("addListener", callback)
+func OnOpenFileRequested(callback func(options js.M, successCallback func(), errorCallback func(err string))) {
+	fileSystemProvider.Get("onOpenFileRequested").Call("addListener", callback)
 }
 
 // OnCloseFileRequested raised when opening a file previously opened with openRequestId is requested to be closed.
-func (f *FileSystemProvider) OnCloseFileRequested(callback func(options Object, successCallback func(), errorCallback func(err string))) {
-	f.o.Get("onCloseFileRequested").Call("addListener", callback)
+func OnCloseFileRequested(callback func(options js.M, successCallback func(), errorCallback func(err string))) {
+	fileSystemProvider.Get("onCloseFileRequested").Call("addListener", callback)
 }
 
 // OnReadFileRequested raised when reading contents of a file opened previously with openRequestId is requested.
 // The results must be returned in chunks by calling successCallback several times. In case of an error, errorCallback must be called.
-func (f *FileSystemProvider) OnReadFileRequested(callback func(options Object, successCallback func(data interface{}, hasMore bool), errorCallback func(err string))) {
-	f.o.Get("onReadFileRequested").Call("addListener", callback)
+func OnReadFileRequested(callback func(options js.M, successCallback func(data interface{}, hasMore bool), errorCallback func(err string))) {
+	fileSystemProvider.Get("onReadFileRequested").Call("addListener", callback)
 }
 
 // OnCreateDirectoryRequested raised when creating a directory is requested. The operation must fail with the EXISTS error
 // if the target directory already exists. If recursive is true, then all of the missing directories on the directory path must be created.
-func (f *FileSystemProvider) OnCreateDirectoryRequested(callback func(options Object, successCallback func(), errorCallback func(err string))) {
-	f.o.Get("onCreateDirectoryRequested").Call("addListener", callback)
+func OnCreateDirectoryRequested(callback func(options js.M, successCallback func(), errorCallback func(err string))) {
+	fileSystemProvider.Get("onCreateDirectoryRequested").Call("addListener", callback)
 }
 
 // OnDeleteEntryRequested raised when deleting an entry is requested. If recursive is true, and the entry is a directory,
 // then all of the entries inside must be recursively deleted as well.
-func (f *FileSystemProvider) OnDeleteEntryRequested(callback func(options Object, successCallback func(), errorCallback func(err string))) {
-	f.o.Get("onDeleteEntryRequested").Call("addListener", callback)
+func OnDeleteEntryRequested(callback func(options js.M, successCallback func(), errorCallback func(err string))) {
+	fileSystemProvider.Get("onDeleteEntryRequested").Call("addListener", callback)
 }
 
 // OnCreateFileReqested raised when creating a file is requested. If the file already exists, then errorCallback must
 // be called with the EXISTS error code.
-func (f *FileSystemProvider) OnCreateFileReqested(callback func(options Object, successCallback func(), errorCallback func(err string))) {
-	f.o.Get("onCreateFileReqested").Call("addListener", callback)
+func OnCreateFileReqested(callback func(options js.M, successCallback func(), errorCallback func(err string))) {
+	fileSystemProvider.Get("onCreateFileReqested").Call("addListener", callback)
 }
 
 // OnCopyEntryRequested raised when copying an entry (recursively if a directory) is requested. If an error occurs,
 // then errorCallback must be called.
-func (f *FileSystemProvider) OnCopyEntryRequested(callback func(options Object, successCallback func(), errorCallback func(err string))) {
-	f.o.Get("onCopyEntryRequested").Call("addListener", callback)
+func OnCopyEntryRequested(callback func(options js.M, successCallback func(), errorCallback func(err string))) {
+	fileSystemProvider.Get("onCopyEntryRequested").Call("addListener", callback)
 }
 
 // OnMoveEntryRequested raised when moving an entry (recursively if a directory) is requested. If an error occurs,
 // then errorCallback must be called.
-func (f *FileSystemProvider) OnMoveEntryRequested(callback func(options Object, successCallback func(), errorCallback func(err string))) {
-	f.o.Get("onMoveEntryRequested").Call("addListener", callback)
+func OnMoveEntryRequested(callback func(options js.M, successCallback func(), errorCallback func(err string))) {
+	fileSystemProvider.Get("onMoveEntryRequested").Call("addListener", callback)
 }
 
 // OnTruncateRequested raised when truncating a file to a desired length is requested. If an error occurs,
 // then errorCallback must be called.
-func (f *FileSystemProvider) OnTruncateRequested(callback func(options Object, successCallback func(), errorCallback func(err string))) {
-	f.o.Get("onTruncateRequested").Call("addListener", callback)
+func OnTruncateRequested(callback func(options js.M, successCallback func(), errorCallback func(err string))) {
+	fileSystemProvider.Get("onTruncateRequested").Call("addListener", callback)
 }
 
 // OnWriteFileRequested raised when writing contents to a file opened previously with openRequestId is requested.
-func (f *FileSystemProvider) OnWriteFileRequested(callback func(options Object, successCallback func(), errorCallback func(err string))) {
-	f.o.Get("onWriteFileRequested").Call("addListener", callback)
+func OnWriteFileRequested(callback func(options js.M, successCallback func(), errorCallback func(err string))) {
+	fileSystemProvider.Get("onWriteFileRequested").Call("addListener", callback)
 }
 
 /* OnAbortRequested raised when aborting an operation with operationRequestId is requested. The operation executed with operationRequestId must be immediately stopped and successCallback of this abort request executed. If aborting fails, then errorCallback must be called. Note, that callbacks of the aborted operation must not be called, as they will be ignored. Despite calling errorCallback, the request may be forcibly aborted. */
-func (f *FileSystemProvider) OnAbortRequested(callback func(options Object, successCallback func(), errorCallback func(err string))) {
-	f.o.Get("onAbortRequested").Call("addListener", callback)
+func OnAbortRequested(callback func(options js.M, successCallback func(), errorCallback func(err string))) {
+	fileSystemProvider.Get("onAbortRequested").Call("addListener", callback)
 }
