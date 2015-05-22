de6e935879c4d124833651a7030aee759e39800d
Archs <gitarchs@yeah.net>
2015-05-23 01:36:01 +0800

    seperate history

--- history/history.go
+++ history/history.go
@@ -1,10 +1,13 @@
-package chrome
+package history
 
-import "github.com/gopherjs/gopherjs/js"
+import (
+	"github.com/Archs/chrome"
+	"github.com/gopherjs/gopherjs/js"
+)
 
-type History struct {
-	o *js.Object
-}
+var (
+	history = chrome.Get("history")
+)
 
 /*
 * Types
@@ -34,34 +37,34 @@ type VisitItem struct {
  */
 
 // Search searches the history for the last visit time of each page matching the query.
-func (h *History) Search(query Object, callback func(results []HistoryItem)) {
-	h.o.Call("search", query, callback)
+func Search(query js.M, callback func(results []HistoryItem)) {
+	history.Call("search", query, callback)
 }
 
 // GetVisits retrieves information about visits to a URL.
-func (h *History) GetVisits(details Object, callback func(results []VisitItem)) {
-	h.o.Call("getVisits", details, callback)
+func GetVisits(details js.M, callback func(results []VisitItem)) {
+	history.Call("getVisits", details, callback)
 }
 
 // AddUrl adds a URL to the history at the current time with a transition type of "link".
-func (h *History) AddUrl(details Object, callback func()) {
-	h.o.Call("addUrl", details, callback)
+func AddUrl(details js.M, callback func()) {
+	history.Call("addUrl", details, callback)
 }
 
 // DeleteUrl removes all occurrences of the given URL from the history.
-func (h *History) DeleteUrl(details Object, callback func()) {
-	h.o.Call("deleteUrl", details, callback)
+func DeleteUrl(details js.M, callback func()) {
+	history.Call("deleteUrl", details, callback)
 }
 
 // DeleteRange removes all items within the specified date range from the history.
 // Pages will not be removed from the history unless all visits fall within the range.
-func (h *History) DeleteRange(rang Object, callback func()) {
-	h.o.Call("deleteRange", rang, callback)
+func DeleteRange(rang js.M, callback func()) {
+	history.Call("deleteRange", rang, callback)
 }
 
 // DeleteAll deletes all items from the history.
-func (h *History) DeleteAll(callback func()) {
-	h.o.Call("deleteAll", callback)
+func DeleteAll(callback func()) {
+	history.Call("deleteAll", callback)
 }
 
 /*
@@ -70,12 +73,12 @@ func (h *History) DeleteAll(callback func()) {
 
 // OnVisited fired when a URL is visited, providing the HistoryItem data for that URL.
 // This event fires before the page has loaded.
-func (h *History) OnVisited(callback func(result HistoryItem)) {
-	h.o.Get("onVisited").Call("addListener", callback)
+func OnVisited(callback func(result HistoryItem)) {
+	history.Get("onVisited").Call("addListener", callback)
 }
 
 // OnVisitedRemoved fired when one or more URLs are removed from the history service.
 // When all visits have been removed the URL is purged from history.
-func (h *History) OnVisitedRemoved(callback func(removed Object)) {
-	h.o.Get("onVisitedRemoved").Call("addListener", callback)
+func OnVisitedRemoved(callback func(removed js.M)) {
+	history.Get("onVisitedRemoved").Call("addListener", callback)
 }
