af626a9b3b524ee0d040a0ad201d797f3a8e14ee
Archs <gitarchs@yeah.net>
2015-05-23 01:37:35 +0800

    seperate i18n

--- i18n/i18n.go
+++ i18n/i18n.go
@@ -1,10 +1,12 @@
-package chrome
+package i18n
 
-import "github.com/gopherjs/gopherjs/js"
+import (
+	"github.com/Archs/chrome"
+)
 
-type I18n struct {
-	o *js.Object
-}
+var (
+	i18n = chrome.Get("i18n")
+)
 
 /*
 * Methods:
@@ -12,20 +14,20 @@ type I18n struct {
 
 // GetAcceptLanguages gets the accept-languages of the browser. This is different from
 // the locale used by the browser; to get the locale, use i18n.getUILanguage.
-func (i *I18n) GetAcceptLanguages(callback func(languages []string)) {
-	i.o.Call("getAcceptLanguages", callback)
+func GetAcceptLanguages(callback func(languages []string)) {
+	i18n.Call("getAcceptLanguages", callback)
 }
 
 // GetMessage gets the localized string for the specified message. If the message is missing,
 // this method returns an empty string (''). If the format of the getMessage() call is wrong —
 // for example, messageName is not a string or the substitutions array has more than 9 elements —
 // this method returns undefined.
-func (i *I18n) GetMessage(messageName string, substitutions interface{}) string {
-	return i.o.Call("getMessage", messageName, substitutions).String()
+func GetMessage(messageName string, substitutions interface{}) string {
+	return i18n.Call("getMessage", messageName, substitutions).String()
 }
 
 // GetUILanguage gets the browser UI language of the browser. This is different from
 // i18n.getAcceptLanguages which returns the preferred user languages.
-func (i *I18n) GetUILanguage() string {
-	return i.o.Call("getUILanguage").String()
+func GetUILanguage() string {
+	return i18n.Call("getUILanguage").String()
 }
