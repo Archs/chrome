55a99afab6f90ab6bb8bdd72d44db3e59fe2bf89
Archs <sun@archs.tk>
2015-05-23 00:25:19 +0800

    seperate cookies

--- cookies/cookies.go
+++ cookies/cookies.go
@@ -1,10 +1,13 @@
-package chrome
+package cookies
 
-import "github.com/gopherjs/gopherjs/js"
+import (
+	"github.com/Archs/chrome"
+	"github.com/gopherjs/gopherjs/js"
+)
 
-type Cookies struct {
-	o *js.Object
-}
+var (
+	cookies = chrome.Get("cookies")
+)
 
 /*
 * Types
@@ -37,30 +40,30 @@ type CookieStore struct {
 // Get retrieves information about a single cookie. If more than one cookie of the same name
 // exists for the given URL, the one with the longest path will be returned. For cookies with
 // the same path length, the cookie with the earliest creation time will be returned.
-func (c *Cookies) Get(details Object, callback func(cookie Cookie)) {
-	c.o.Call("get", details, callback)
+func Get(details js.M, callback func(cookie Cookie)) {
+	cookies.Call("get", details, callback)
 }
 
 // GetAll retrieves all cookies from a single cookie store that match the given information.
 // The cookies returned will be sorted, with those with the longest path first. If multiple cookies
 // have the same path length, those with the earliest creation time will be first.
-func (c *Cookies) GetAll(details Object, callback func(cookies []Cookie)) {
-	c.o.Call("getAll", details, callback)
+func GetAll(details js.M, callback func(cookies []Cookie)) {
+	cookies.Call("getAll", details, callback)
 }
 
 // Set sets a cookie with the given cookie data; may overwrite equivalent cookies if they exist.
-func (c *Cookies) Set(details Object, callback func(cookie Cookie)) {
-	c.o.Call("set", details, callback)
+func Set(details js.M, callback func(cookie Cookie)) {
+	cookies.Call("set", details, callback)
 }
 
 // Remove deletes a cookie by name.
-func (c *Cookies) Remove(details Object, callback func(details Object)) {
-	c.o.Call("remove", details, callback)
+func Remove(details js.M, callback func(details js.M)) {
+	cookies.Call("remove", details, callback)
 }
 
 // GetAllCookieStores lists all existing cookie stores.
-func (c *Cookies) GetAllCookieStores(callback func(cookieStores []CookieStore)) {
-	c.o.Call("getAllCookieStores", callback)
+func GetAllCookieStores(callback func(cookieStores []CookieStore)) {
+	cookies.Call("getAllCookieStores", callback)
 }
 
 /*
@@ -71,6 +74,6 @@ func (c *Cookies) GetAllCookieStores(callback func(cookieStores []CookieStore)) 
 //  is implemented as a two step process: the cookie to be updated is first removed entirely, generating a
 // notification with "cause" of "overwrite" . Afterwards, a new cookie is written with the updated values,
 // generating a second notification with "cause" "explicit".
-func (c *Cookies) OnChanged(callback func(changeInfo Object)) {
-	c.o.Get("onChanged").Call("addListener", callback)
+func OnChanged(callback func(changeInfo js.M)) {
+	cookies.Get("onChanged").Call("addListener", callback)
 }
