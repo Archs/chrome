15c01353bfffc8b915b36eb0d807fc835ebeb6e0
Archs <gitarchs@yeah.net>
2015-05-23 01:05:37 +0800

    seperate declarativeContent

--- declarativeContent/declarative-content.go
+++ declarativeContent/declarative-content.go
@@ -1,29 +1,22 @@
-package chrome
+package declarativeContent
 
 import (
-	"fmt"
-
+	"github.com/Archs/chrome"
 	"github.com/gopherjs/gopherjs/js"
 )
 
-type DeclarativeContent struct {
-	o             *js.Object
-	OnPageChanged *OnPageChanged //Need to remove this and fix methods below...
-}
+var (
+	declarativeContent = chrome.Get("declarativeContent")
+	OnPageChanged      = newOnPageChangedEvent(declarativeContent.Get("onPageChanged"))
+)
 
-type OnPageChanged struct {
+type OnPageChangedEvent struct {
 	o *js.Object
 }
 
-func NewDeclarativeContent(declarativeContentObj *js.Object) *DeclarativeContent {
-	d := new(DeclarativeContent)
-	d.o = declarativeContentObj
-	fmt.Println(d.o.String())
-	if d.o.String() != "undefined" {
-		d.OnPageChanged = &OnPageChanged{o: d.o.Get("onPageChanged")}
-	} else {
-		d.OnPageChanged = &OnPageChanged{o: nil}
-	}
+func newOnPageChangedEvent(o *js.Object) *OnPageChangedEvent {
+	d := new(OnPageChangedEvent)
+	d.o = o
 	return d
 }
 
@@ -32,13 +25,13 @@ func NewDeclarativeContent(declarativeContentObj *js.Object) *DeclarativeContent
  */
 
 type PageStateMatcher struct {
-	js.Object
-	PageUrl Object   `js:"pageUrl"`
+	*js.Object
+	PageUrl js.M     `js:"pageUrl"`
 	Css     []string `js:"css"`
 }
 
 type RequestContentScript struct {
-	js.Object
+	*js.Object
 	Css             []string `js:"css"`
 	Js              []string `js:"js"`
 	AllFrames       bool     `js:"allFrames"`
@@ -50,16 +43,16 @@ type RequestContentScript struct {
  */
 
 // AddRules takes an array of rule instances as its first parameter and a callback function that is called on completion
-func (e *OnPageChanged) AddRules(rules []map[string]interface{}, callback func(details map[string]interface{})) {
+func (e *OnPageChangedEvent) AddRules(rules []map[string]interface{}, callback func(details map[string]interface{})) {
 	e.o.Call("addRules", rules, callback)
 }
 
 // RemoveRules accepts an optional array of rule identifiers as its first parameter and a callback function as its second parameter.
-func (e *OnPageChanged) RemoveRules(ruleIdentifiers []string, callback func()) {
+func (e *OnPageChangedEvent) RemoveRules(ruleIdentifiers []string, callback func()) {
 	e.o.Call("removeRules", ruleIdentifiers, callback)
 }
 
 // GetRules accepts an optional array of rule identifiers with the same semantics as removeRules and a callback function.
-func (e *OnPageChanged) GetRules(ruleIdentifiers []string, callback func(details map[string]interface{})) {
+func (e *OnPageChangedEvent) GetRules(ruleIdentifiers []string, callback func(details map[string]interface{})) {
 	e.o.Call("getRules", ruleIdentifiers, callback)
 }
